817fa5cb1e87a2f8635784bb7a29274b7fe96cad
Tinymakesstuff <tinymakesstuff@gmail.com>
2025-12-19 22:35:50 -0500

    CAGE HEALTH WHOO

---
+++ Assets/Dev/Sean/CageManager.cs
@@ -0,0 +1,20 @@
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class CageManager : MonoBehaviour
+{
+    public float cageHealth = 100;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    
+}
