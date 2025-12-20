c481df01f7000bf300d5aafe93637b92708bffb4
Tinymakesstuff <tinymakesstuff@gmail.com>
2025-12-20 11:32:31 -0500

    health and mana bar

---
+++ Assets/Dev/Sean/SliderHandlers.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SliderHandlers : MonoBehaviour
+{
+    public PlayerController pc;
+    public Slider healthBar;
+    public Slider manaBar;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        healthBar.value = pc.health / pc.maxHealth;
+    }
+}
