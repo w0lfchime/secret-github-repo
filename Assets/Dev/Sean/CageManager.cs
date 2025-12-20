bd628c17d66cf86a73ae1864f6d12f51e8e87b60
Tinymakesstuff <tinymakesstuff@gmail.com>
2025-12-19 22:44:24 -0500

    You can now game over if your a fucking LOSER

--- Assets/Dev/Sean/CageManager.cs
+++ Assets/Dev/Sean/CageManager.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 public class CageManager : MonoBehaviour
 {
     public float cageHealth = 100;
+    public PlayerController pc;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -13,7 +14,10 @@ public class CageManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if(cageHealth <= 0)
+        {
+            pc.gameOver();
+        }
     }
 
     
