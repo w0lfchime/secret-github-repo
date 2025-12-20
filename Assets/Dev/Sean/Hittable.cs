2b1cfdab45b16402c93816bd785cebf58f14422b
frogggggggg <139102704+frogggggggg@users.noreply.github.com>
2025-12-20 17:54:51 -0500

    Ice Stuff

--- Assets/Dev/Sean/Hittable.cs
+++ Assets/Dev/Sean/Hittable.cs
@@ -5,8 +5,10 @@ using UnityEngine.Rendering;
 public class Hittable : MonoBehaviour
 {
     public CageManager cm;
+    public Renderer rend;
     public bool attackingCage = false;
     public float health = 10;
+    public float iced;
     public float damage = 5;
     public float attackSpeed = 1f;
     public int expAmount;
@@ -20,7 +22,11 @@ public class Hittable : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        MaterialPropertyBlock mpb = new MaterialPropertyBlock();
+
+        rend.GetPropertyBlock(mpb);
+        mpb.SetFloat("_Alpha", iced);
+        rend.SetPropertyBlock(mpb);
     }
 
     public void startEnemyCageAttack()
