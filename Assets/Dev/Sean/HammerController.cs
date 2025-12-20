58d3059544dcfa9895f5db54c1d95d7bece1718b
Tinymakesstuff <tinymakesstuff@gmail.com>
2025-12-19 20:25:55 -0500

    hammer does damage

--- Assets/Dev/Sean/HammerController.cs
+++ Assets/Dev/Sean/HammerController.cs
@@ -3,16 +3,19 @@ using UnityEngine;
 public class HammerController : MonoBehaviour
 {
     public float damage = 5f;
-    public float speed = 5f;
+    public float speed = 0f;
     public Camera mainCam;
     public GameObject player;
+    public Rigidbody rb;
     private Vector3 mousePosition;
+    private Vector3 previousDirection;
     public LayerMask groundLayer;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         Vector3 initialDirection = transform.position - player.transform.position;
         transform.position = player.transform.position + initialDirection.normalized * 5;
+        previousDirection = transform.position;
     }
 
     // Update is called once per frame
@@ -37,10 +40,25 @@ public class HammerController : MonoBehaviour
             movement.y = player.transform.position.y;
 
             transform.position = movement;
+
+            speed = Vector3.Distance(transform.position, previousDirection) / Time.deltaTime;
+            previousDirection = transform.position;
         }
     }
 
     public void OnCollisionEnter(Collision col) {
+        Debug.Log("Collision entered");
 
+        if(col.gameObject.tag == "Enemy")
+        {
+            if(speed > 5)
+            {
+                Debug.Log("Hit an enemy with velovity: " + speed);
+                Hittable hit = col.gameObject.GetComponent<Hittable>();
+
+                hit.health -= damage;
+            }
+            
+        }
     }
 }
