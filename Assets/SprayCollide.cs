3351eb56b14ad976b15cb2db9b7a4f171d81f412
frogggggggg <139102704+frogggggggg@users.noreply.github.com>
2025-12-20 22:29:16 -0500

    Beam rework

---
+++ Assets/SprayCollide.cs
@@ -0,0 +1,35 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(ParticleSystem))]
+public class SprayCollide : MonoBehaviour
+{
+    [Min(0f)] public float power = 0.01f;
+
+    private ParticleSystem ps;
+    private List<ParticleCollisionEvent> collisionEvents;
+
+    void Awake()
+    {
+        ps = GetComponent<ParticleSystem>();
+        collisionEvents = new List<ParticleCollisionEvent>(32); // pre-size for fewer reallocs
+    }
+
+    void OnParticleCollision(GameObject other)
+    {
+        // Ultra-safe in case something weird disables/enables order
+        if (collisionEvents == null)
+            collisionEvents = new List<ParticleCollisionEvent>(32);
+
+        if (!other.CompareTag("Enemy"))
+            return;
+
+        int num = ps.GetCollisionEvents(other, collisionEvents);
+        if (num <= 0)
+            return;
+
+        var hit = other.GetComponentInParent<Hittable>();
+        if (hit != null)
+            hit.iced = Mathf.Clamp(hit.iced + num * power, 0, 1);
+    }
+}
