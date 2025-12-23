a04c15ffa99370afb9632bfcb0f0b80e986366af
Tinymakesstuff <tinymakesstuff@gmail.com>
2025-12-23 14:33:24 -0500

    item system rework

--- Assets/Dev/Nic/Scripts/Item.cs
+++ Assets/Dev/Nic/Scripts/Item.cs
@@ -1,3 +1,6 @@
+using System;
+using System.Collections.Generic;
+using System.Reflection;
 using UnityEngine;
 
 [CreateAssetMenu(fileName = "New Item", menuName = "Inventory/Item")]
@@ -26,4 +29,90 @@ public class Item : ScriptableObject
     // 14: life steal
     public int itemType;
     public float increaseBy;
+    public AlexHammerCopy hc;
+    public PlayerController pc;
+    public CageManager cm;
+    public List<float> values;
+    public List<String> toChange;
+
+    public void addEffect()
+    {
+        Type item = this.GetType();
+
+        for(int i = 0; i < values.Count; i++)
+        {
+            var method = this.GetType().GetMethod(toChange[i]);
+            var args = new object[] {values[i]};
+
+            method.Invoke(this, args);
+        }
+    }
+
+    public void coneSize(float changeBy)
+    {
+        var shape = GameObject.Find("FramePivot").transform.GetChild(0).GetComponent<ParticleSystem>().shape;
+        shape.angle += changeBy;
+    }
+
+    public void playerSpeed(float changeBy)
+    {
+        hc.speedMulti += changeBy;
+    }
+
+    public void damage(float changeBy)
+    {
+        hc.damage += changeBy;
+    }
+
+    public void cageHealth(float changeBy)
+    {
+        cm.maxHealth += changeBy;
+    }
+
+    public void playerHealth(float changeBy)
+    {
+        pc.maxHealth += changeBy;
+        pc.health += changeBy;
+    }
+
+    public void playerRegen(float changeBy)
+    {
+        pc.regenTime += changeBy;
+
+        if(pc.regenTime < 0.1)
+        {
+            pc.regenTime = 0.1f;
+        }
+    }
+
+    public void extraExp(float changeBy)
+    {
+        hc.additionalExp += changeBy;
+    }
+
+    public void maxMana(float changeBy)
+    {
+        pc.maxMana += changeBy;
+        pc.mana += changeBy;
+    }
+
+    public void hammerSize(float changeBy)
+    {
+        hc.changeHammerScale(changeBy);
+    }
+
+    public void hammerSpeedLimit(float changeBy)
+    {
+        hc.limit += changeBy;
+    }
+
+    public void spinMulti(float changeBy)
+    {
+        hc.spinMultiplier += changeBy;
+    }
+
+    public void lifeSteal(float changeBy)
+    {
+        hc.lifeSteal += changeBy;
+    }
 }
--- Assets/Dev/Nic/Scripts/ItemSelectionUI.cs
+++ Assets/Dev/Nic/Scripts/ItemSelectionUI.cs
@@ -69,56 +69,62 @@ public class ItemSelectionUI : MonoBehaviour
         
         if (selectedItem != null)
         {
-            if(selectedItem.itemType == 1)
-            {
-                var shape = particleStuff.GetComponent<ParticleSystem>().shape;
-                shape.angle += selectedItem.increaseBy;
-            } else if (selectedItem.itemType == 2)
-            {
-                hc.speedMulti += selectedItem.increaseBy;
-            } else if (selectedItem.itemType == 3)
-            {
-                hc.damage += selectedItem.increaseBy;
-            } else if (selectedItem.itemType == 4)
-            {
-                cm.maxHealth += selectedItem.increaseBy;
-            } else if (selectedItem.itemType == 5)
-            {
-                pc.maxHealth += selectedItem.increaseBy;
-                pc.health += selectedItem.increaseBy;
-            } else if (selectedItem.itemType == 6)
-            {
-                pc.regenTime -= selectedItem.increaseBy;
-                if(pc.regenTime < 0.1)
-                {
-                    pc.regenTime = 0.1f;
-                }
-            } else if (selectedItem.itemType == 7)
-            {
-                hc.additionalExp += selectedItem.increaseBy;
-            } else if (selectedItem.itemType == 8)
-            {
-                pc.maxMana += selectedItem.increaseBy;
-                pc.mana += selectedItem.increaseBy;
-            } else if (selectedItem.itemType == 9)
-            {
-                pc.manaRegenTime -= selectedItem.increaseBy;
-            } else if (selectedItem.itemType == 10)
-            {
+            // if(selectedItem.itemType == 1)
+            // {
+            //     var shape = particleStuff.GetComponent<ParticleSystem>().shape;
+            //     shape.angle += selectedItem.increaseBy;
+            // } else if (selectedItem.itemType == 2)
+            // {
+            //     hc.speedMulti += selectedItem.increaseBy;
+            // } else if (selectedItem.itemType == 3)
+            // {
+            //     hc.damage += selectedItem.increaseBy;
+            // } else if (selectedItem.itemType == 4)
+            // {
+            //     cm.maxHealth += selectedItem.increaseBy;
+            // } else if (selectedItem.itemType == 5)
+            // {
+            //     pc.maxHealth += selectedItem.increaseBy;
+            //     pc.health += selectedItem.increaseBy;
+            // } else if (selectedItem.itemType == 6)
+            // {
+            //     pc.regenTime -= selectedItem.increaseBy;
+            //     if(pc.regenTime < 0.1)
+            //     {
+            //         pc.regenTime = 0.1f;
+            //     }
+            // } else if (selectedItem.itemType == 7)
+            // {
+            //     hc.additionalExp += selectedItem.increaseBy;
+            // } else if (selectedItem.itemType == 8)
+            // {
+            //     pc.maxMana += selectedItem.increaseBy;
+            //     pc.mana += selectedItem.increaseBy;
+            // } else if (selectedItem.itemType == 9)
+            // {
+            //     pc.manaRegenTime -= selectedItem.increaseBy;
+            // } else if (selectedItem.itemType == 10)
+            // {
                 
-            } else if(selectedItem.itemType == 11)
-            {
-                hc.limit += selectedItem.increaseBy;
-            } else if(selectedItem.itemType == 12)
-            {
-                hc.spinMultiplier += selectedItem.increaseBy;
-            } else if(selectedItem.itemType == 13)
-            {
-                hc.changeHammerScale(selectedItem.increaseBy);
-            } else if(selectedItem.itemType == 14)
-            {
-                hc.lifeSteal += selectedItem.increaseBy;
-            }
+            // } else if(selectedItem.itemType == 11)
+            // {
+            //     hc.limit += selectedItem.increaseBy;
+            // } else if(selectedItem.itemType == 12)
+            // {
+            //     hc.spinMultiplier += selectedItem.increaseBy;
+            // } else if(selectedItem.itemType == 13)
+            // {
+            //     hc.changeHammerScale(selectedItem.increaseBy);
+            // } else if(selectedItem.itemType == 14)
+            // {
+            //     hc.lifeSteal += selectedItem.increaseBy;
+            // }
+
+            selectedItem.pc = pc;
+            selectedItem.hc = hc;
+            selectedItem.cm = cm;
+            selectedItem.addEffect();
+
             // Invoke callback
             onItemSelectedCallback?.Invoke(selectedItem);
 
